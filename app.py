13a222296fece85b2807e6006045d3cafaf2b049
shielh <shiel.helen@gmail.com>
2021-06-14 18:51:54 +0000

    Adjust styling of navbar

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def error_not_found(error):
     """
     Handles route to 404 error
     """
-    return render_template("404.html", error=error)
+    return render_template("404.html", error=error), 404
 
 
 @app.errorhandler(500)
@@ -303,7 +303,7 @@ def server_error(error):
     """
     Handles route to 500 error
     """
-    return render_template("500.html", error=error)
+    return render_template("500.html", error=error), 500
 
 
 if __name__ == "__main__":
